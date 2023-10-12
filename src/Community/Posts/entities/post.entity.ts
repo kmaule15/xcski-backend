aee8acbf95ed4717a7d169fa4d778a37711bf26a
MattReas <mattreas52@gmail.com>
2023-10-11 19:39:24 -0500

    Created the basics for Posts

---
+++ src/Community/Posts/entities/post.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm"
+
+@Entity('posts')
+export class Post {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    title: string
+
+    @Column('text')
+    content: string
+    
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+}
---
+++ src/Community/Posts/post.service.ts
@@ -0,0 +1,39 @@
+// post.service.ts
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './entities/post.entity';
+import { CreatePostDto } from './dtos/create-post.dto';
+import { UpdatePostDto } from './dtos/update-post.dto';
+
+@Injectable()
+export class PostService {
+  constructor(
+    @InjectRepository(Post)
+    private postRepository: Repository<Post>,
+  ) {}
+
+  findAll(): Promise<Post[]> {
+    return this.postRepository.find();
+  }
+
+  findOne(id: number): Promise<Post> {
+    return this.postRepository.findOne(id);
+  }
+
+  async create(createPostDto: CreatePostDto): Promise<Post> {
+    const post = new Post();
+    post.title = createPostDto.title;
+    post.content = createPostDto.content;
+
+    return this.postRepository.save(post);
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto): Promise<void> {
+    await this.postRepository.update(id, updatePostDto);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.postRepository.delete(id);
+  }
+}
